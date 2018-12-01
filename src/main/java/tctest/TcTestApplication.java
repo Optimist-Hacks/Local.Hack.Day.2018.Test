0ebaabb498be69359fc26054d3a317b4b658de27
otopba <otopba@inbox.ru>
2018-12-01 16:28:59 +0300

    Initial commit

---
+++ src/main/java/tctest/TcTestApplication.java
@@ -0,0 +1,14 @@
+package tctest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcTestApplication.class, args);
+    }
+
+}
