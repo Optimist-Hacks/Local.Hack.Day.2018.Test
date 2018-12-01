2ed31555d9b7b6fa34682171f9c72e674eeb3512
otopba <otopba@inbox.ru>
2018-12-01 19:31:29 +0300

    fasafsdfsadasfd

--- src/main/java/tctest/TcTestApplication.java
+++ src/main/java/tctest/TcTestApplication.java
@@ -10,7 +10,7 @@ public class TcTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TcTestApplication.class, args);
         kekeqweqasdasdadsdasad
-        fads
+        fadsfasfsdaf
     }
 
 }
