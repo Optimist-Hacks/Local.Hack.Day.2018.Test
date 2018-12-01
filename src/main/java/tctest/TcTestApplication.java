2fb6850ce4aec43961aba316ed343c21f8ba2984
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-12-01 21:10:54 +0300

    KEKasda

--- src/main/java/tctest/TcTestApplication.java
+++ src/main/java/tctest/TcTestApplication.java
@@ -1,14 +1,9 @@
 package tctest;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TcTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TcTestApplication.class, args);
-        kekeqweqasdasdadsdasadasdasda
-    }jhvjhvkjhgjgjhjhgjhgjhgjgjhghuyuyyuyugyugyguygu
+//        SpringApplication.run(TcTestApplication.class, args);
+    }
 }
